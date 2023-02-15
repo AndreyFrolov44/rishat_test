8363b16d0a355296be5128d027d6556f7ff84080
Andreyfrolov44 <andre.frolov44@gmail.com>
2023-02-15 16:47:10 +0300

    bonus tasks

--- api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from api.models import Item
-
-
-@admin.register(Item)
-class YourModelAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price',)
--- api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import item, buy, cancel, success
-
-urlpatterns = [
-    path('item/<int:item_id>/', item, name='item'),
-    path('buy/<int:item_id>/', buy, name='buy'),
-    path('cancel/', cancel, name='cancel'),
-    path('success/', success, name='success'),
-]
